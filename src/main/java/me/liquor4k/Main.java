147505a4280e6438cd0918da2d544832e4ea49a2
Софья <kiratuber01@gmail.com>
2025-09-19 15:56:25 +0300

    Задание 3

--- src/main/java/me/liquor4k/Main.java
+++ src/main/java/me/liquor4k/Main.java
@@ -19,5 +19,14 @@ public class Main {
         } else {
             System.out.println("На улице " + temperature + " градусов, можно идти без шапки");
         }
+
+        // Задача 3: Проверка скорости
+        System.out.println("\n=== ЗАДАЧА 3 ===");
+        int speed = 65;
+        if (speed > 60) {
+            System.out.println("Если скорость " + speed + ", то придется заплатить штраф");
+        } else {
+            System.out.println("Если скорость " + speed + ", то можно ездить спокойно");
+        }
     }
 }
