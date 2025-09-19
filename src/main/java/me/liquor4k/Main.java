695ea4cc670aa7bc8dbc7bf845fa857115f5f71e
Софья <kiratuber01@gmail.com>
2025-09-19 15:55:23 +0300

    Задание 2

--- src/main/java/me/liquor4k/Main.java
+++ src/main/java/me/liquor4k/Main.java
@@ -1,14 +1,23 @@
 package me.liquor4k;
 
-//задача 1
 public class Main {
     public static void main(String[] args) {
-        int age = 20;
-
-        if (age >= 18) {
-            System.out.println("Если возраст человека равен " + age + ", то он совершеннолетний");
+        // Задача 1: Проверка совершеннолетия
+        System.out.println("=== ЗАДАЧА 1 ===");
+        int age1 = 20;
+        if (age1 >= 18) {
+            System.out.println("Если возраст человека равен " + age1 + ", то он совершеннолетний");
         } else {
-            System.out.println("Если возраст человека равен " + age + ", то он не достиг совершеннолетия");
+            System.out.println("Если возраст человека равен " + age1 + ", то он не достиг совершеннолетия, нужно немного подождать");
+        }
+
+        // Задача 2: Проверка температуры для шапки
+        System.out.println("\n=== ЗАДАЧА 2 ===");
+        int temperature = 3;
+        if (temperature < 5) {
+            System.out.println("На улице " + temperature + " градусов, нужно надеть шапку");
+        } else {
+            System.out.println("На улице " + temperature + " градусов, можно идти без шапки");
         }
     }
 }
