ebd453d5aadce7e3ebf34b8bd02912a3f26085df
Софья <kiratuber01@gmail.com>
2025-09-19 15:47:00 +0300

    Задание 1

--- src/main/java/me/liquor4k/Main.java
+++ src/main/java/me/liquor4k/Main.java
@@ -1,2 +1,14 @@
 package me.liquor4k;
 
+//задача 1
+public class Main {
+    public static void main(String[] args) {
+        int age = 20;
+
+        if (age >= 18) {
+            System.out.println("Если возраст человека равен " + age + ", то он совершеннолетний");
+        } else {
+            System.out.println("Если возраст человека равен " + age + ", то он не достиг совершеннолетия");
+        }
+    }
+}
